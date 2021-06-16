1d2f7f0caa07324a449be08c6c192fab1feb8f87
kang <rilac1@naver.com>
2021-06-16 20:01:33 +0900

    0616

--- Chapter13_DFS BFS 문제/Q16_연구소.py
+++ Chapter13_DFS BFS 문제/Q16_연구소.py
@@ -1,3 +1,72 @@
+from collections import deque
+import itertools
+
+def bfs(graph,x,y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx<0 or ny<0 or nx>=n or ny>=m:
+                continue
+            if graph[nx][ny] == 1:
+                continue
+            if graph[nx][ny] == 0:
+                queue.append((nx,ny))
+                graph[nx][ny] = 2
+
+def test(graph):
+    count = 0
+    for i in virus:
+        bfs(graph, i[0], i[1])
+
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j]==0:
+                count += 1
+    return count
+
+# n, m = map(int, input().split())
+n, m = 7, 7
+graph = []
+graph = [
+    [2,0,0,0,1,1,0],
+    [0,0,1,0,1,2,0],
+    [0,1,1,0,1,0,0],
+    [0,1,0,0,0,0,0],
+    [0,0,0,0,0,1,1],
+    [0,1,0,0,0,0,0],
+    [0,1,0,0,0,0,0]
+]
+'''
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+'''
+
+virus = []
+for i in range(n):
+    for j in range(m):
+        if graph[i][j]==2:
+            virus.append((i,j))
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+result = 0
+for i in itertools.permutations(list(range(n*m)),3):
+    t_graph = graph
+    for j in i:
+        x = j//n
+        y = j % m
+        if t_graph[x][y]==0:
+            break
+        t_graph[x][y] = 1
+    result = max(result, test(t_graph))
+
+print(result)
+
 '''
 7 7
 2 0 0 0 1 1 0
@@ -8,40 +77,3 @@
 0 1 0 0 0 0 0
 0 1 0 0 0 0 0
 '''
-
-from collections import deque
-
-n, m = map(int, input().split())
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input().split())))
-
-virus = []
-for i in range(n):
-    for j in range(m):
-        if graph[n][m]==2:
-            virus.append((n,m))
-
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-
-def bfs(x,y):
-    queue = deque((x,y))
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            dx = x + dx[i]
-            dy = x + dy[i]
-            if dx<0 or dy<0 or dx>=n or dy>=m:
-                continue
-            if graph[dx][dy] == 1:
-                continue
-            if graph[dx][dy] == 0:
-                queue.append((dx,dy))
-                graph[dx][dy] = 2
-
-for i in virus:
-    bfs(i)
-
-for i in graph:
-    print(i)
