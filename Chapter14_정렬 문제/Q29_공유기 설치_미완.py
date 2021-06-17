0e28a6505d2b54c02a0986be37c0d4afad0dbd48
kang <rilac1@naver.com>
2021-06-17 20:15:25 +0900

    0617

---
+++ Chapter14_정렬 문제/Q29_공유기 설치_미완.py
@@ -0,0 +1,6 @@
+n, c = map(int, input().split())
+data = []
+for _ in range(n):
+    data.append(int(input()))
+
+data.sort()
---
+++ Chapter15_이진탐색 문제/Q27_특정 수 개수 구하기.py
@@ -0,0 +1,3 @@
+n, x = map(int, input().split())
+data = list(map(int, input().split()))
+print(data.count(x))
