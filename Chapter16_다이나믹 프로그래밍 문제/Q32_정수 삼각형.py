4699d9802b7828f2be2c3b84fb184cbc456a9da7
kang <rilac1@naver.com>
2021-06-18 15:52:44 +0900

    210618

--- Chapter16_다이나믹 프로그래밍 문제/Q32_정수 삼각형.py
+++ Chapter16_다이나믹 프로그래밍 문제/Q32_정수 삼각형.py
@@ -1,4 +1,16 @@
 n = int(input())
 data = []
-for _ in range(n):
-    data.append(int(map, input().split))
+data.append(int(input()))
+for _ in range(1,n):
+    data.append(list(map(int, input().split())))
+
+data[1][0] += data[0]
+data[1][1] += data[0]
+
+for i in range(2,n):
+    data[i][0] += data[i-1][0]
+    for j in range(1,i):
+        data[i][j] += max(data[i-1][j], data[i-1][j-1])
+    data[i][i] += data[i-1][i-1]
+
+print(max(data[n-1]))
